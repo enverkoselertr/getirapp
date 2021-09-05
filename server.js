1f0f0b311bcc3edf47a38e143b69b5adf73c26ee
enverkoselertr <enverkoselertr@gmail.com>
2021-09-05 18:16:44 +0300

    server

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-// const jsonServer = require('json-server');
-// const server = jsonServer.create();
-// const router = jsonServer.router('./db.json');
-// const middlewares = jsonServer.defaults({
-//   static: './build'
-// });
-// const PORT = process.env.PORT || 3000;
-// server.use(middlewares);
-// server.use(jsonServer.rewriter({
-//   '/api/*': '/$1',
-// }))
-// server.use(router);
-// server.listen(PORT, () => {
-//   console.log('Server is running');
-// });
-// server.js
-const jsonServer = require("json-server");
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults();
-
-server.use(middlewares);
-
-// Have all URLS prefixed with a /api
-server.use(
-  jsonServer.rewriter({
-    "/api/*": "/$1",
-  })
-);
-
-server.use(router);
-server.listen(5000, () => {
-  console.log("JSON Server is running");
+const router = jsonServer.router('./db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
 });
+const PORT = process.env.PORT || 3000;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
+server.use(router);
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
+// server.js
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+// const router = jsonServer.router("db.json");
+// const middlewares = jsonServer.defaults();
+
+// server.use(middlewares);
+
+// // Have all URLS prefixed with a /api
+// server.use(
+//   jsonServer.rewriter({
+//     "/api/*": "/$1",
+//   })
+// );
+
+// server.use(router);
+// server.listen(5000, () => {
+//   console.log("JSON Server is running");
+// });

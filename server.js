98fdfc6061f09d47c95c97f206fe22f63acbeecb
enverkoselertr <enverkoselertr@gmail.com>
2021-09-05 06:46:44 +0300

    server

--- server.js
+++ server.js
@@ -21,11 +21,14 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
 }))
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+// server.listen(port);
+server.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });

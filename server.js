e077b9cf6a994f7f64e4014080109688331f6ca9
enverkoselertr <enverkoselertr@gmail.com>
2021-09-05 07:28:20 +0300

    server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./db.json'); //./src/server/db.js
-const PORT = process.env.PORT || 3000; 
+const router = jsonServer.router('db.json'); //./src/server/db.js
+const PORT = process.env.PORT || 3200; 
 const middlewares = jsonServer.defaults({
   static: './build'
 });
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
 
 server.use(router);
 server.use(middlewares);

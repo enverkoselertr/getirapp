8f87110b401762447aea6f43f339adb34c323b3c
enverkoselertr <enverkoselertr@gmail.com>
2021-09-05 05:30:48 +0300

    server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json'); //./src/server/db.js
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 3000; 
 const middlewares = jsonServer.defaults({
   static: './build'
 });
